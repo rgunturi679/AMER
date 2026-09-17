c1d472f0e007b67e53682eae673655717030e150
Gowtham Gunturi <rgunturi@aligntech.com>
2026-09-17 15:14:56 +0530

    Revert "hold time"
    This reverts commit 7278602454d615512d17f4b3805d7a799acd5899.

--- plugin-flex-ts-template-v2/src/feature-library/hold-time/config.ts
+++
@@ -1,8 +0,0 @@
-import { getFeatureFlags } from '../../utils/configuration';
-import HoldTimeConfig from './types/ServiceConfiguration';
-
-const { enabled = false } = (getFeatureFlags()?.features?.hold_time as HoldTimeConfig) || {};
-
-export const isFeatureEnabled = () => {
-  return enabled;
-};
